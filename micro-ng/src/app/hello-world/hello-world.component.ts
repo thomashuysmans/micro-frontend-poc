9b0345bdaef97b44971ed6193182bcab94346239
Thomas Huysmans <thomas@kaizenit.consulting>
2020-08-09 12:56:39 +0200

    Added input/output angular

--- micro-ng/src/app/hello-world/hello-world.component.ts
+++ micro-ng/src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
@@ -8,9 +8,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class HelloWorldComponent implements OnInit {
 
+
+  @Input() name: string;
+
+  @Output() MessageEvent: EventEmitter<string> = new EventEmitter();
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  onClick() : void {
+    this.MessageEvent.emit("This is a messsage from the Angular component");
   }
 
 }
