ed89f61e242122598eef59e33a979bdfc3f8098d
Thomas Huysmans <thomas@kaizenit.consulting>
2020-08-10 16:23:09 +0200

    Fixed the loading issue for app2

--- micro-ng-app2/src/app/app.module.ts
+++ micro-ng-app2/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from  './helloworld/helloworld.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+    //AppRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [
+    AppComponent,
+    HelloWorldComponent
+  ]
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor(private injector: Injector) {}
+
+   ngDoBootstrap() {
+      const { injector } = this;
+      const ngCustomElement = createCustomElement(HelloWorldComponent, { injector });
+
+      customElements.define('ng-app2-el', ngCustomElement);
+   }
+
+}
---
+++ micro-ng-app2/src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app2-hello-world',
+  templateUrl: './helloworld.component.html',
+  styleUrls: ['./helloworld.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
