d2ce4d93548522497c1b789633e281d6ef481640
Thomas Huysmans <thomas@kaizenit.consulting>
2020-08-10 20:03:02 +0200

    Added search component

--- micro-ng-search/src/app/app.module.ts
+++ micro-ng-search/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
+import { SearchComponent } from './search/search.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [],
+  entryComponents: [
+    AppComponent,
+    SearchComponent
+  ]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const { injector } = this;
+    const ngCustomElement = createCustomElement(SearchComponent, { injector });
+
+    customElements.define('ng-search-el', ngCustomElement);
+ }
+
+}
---
+++ micro-ng-search/src/app/search/search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+// TODO: the styling of this component needs to be done here.
+// For this POC, it will inherit it from the shell application which contains bootstrap
+
+@Component({
+    selector: 'app-search',
+    templateUrl: 'search.component.html'
+})
+
+export class SearchComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
